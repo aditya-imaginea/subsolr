da598ba7efa42189060ce27b381a767867617d31
aditya <aditya25120@gmail.com>
2014-08-13 11:51:14 +0530

    deleting .proceduredocument folder

---
+++ subsolr/src/main/java/com/subsolr/utils/FieldMergeManager.java
@@ -0,0 +1,12 @@
+package com.subsolr.utils;
+
+import java.util.List;
+import java.util.Map;
+
+public final class FieldMergeManager {
+
+	public static List<Map> getMergedFields(String key, List<Map>... fieldSets) {
+		
+		return null;
+	}
+}
